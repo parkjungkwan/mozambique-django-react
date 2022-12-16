46c9ca1d04fd9ba6ec686c49065248aa94cf2d33
parkjungkwan <pakjkwan@gmail.com>
2022-12-16 15:56:03 +0900

    16

--- DjangoServer/api/dlearn/fashion_view.py
+++ DjangoServer/api/dlearn/fashion_view.py
@@ -12,24 +12,12 @@ from api.dlearn.fashion_service import FashionService
 
 @api_view(["GET"])
 def fashion(request, id):
-    print(" ######## GET at Here ! ########  ")
-    # body = request.body  # byte string of JSON data
-    # print(f" ######## request.body is {body} ########  ")
-    # data = json.loads(body)  # json to dict
-    # print(request.headers)  # request의 header 정보
-    # print(request.content_type)  # application/json
-    print(f"######## React ID is {request.GET['id']} ########")
-    resp = FashionService().service_model(int(request.GET['id']))
-    return JsonResponse({'result': resp})
+    print(f"######## GET at Here ! React ID is {request.GET['id']} ########")
+    return JsonResponse(
+        {'result': FashionService().service_model(int(request.GET['id']))})
 
 @api_view(["POST"])
 def fashion(request):
-    print(" ######## POST at Here ! ########  ")
-    body = request.body  # byte string of JSON data
-    print(f" ######## request.body is {body} ########  ")
-    data = json.loads(body)  # json to dict
-    print(request.headers)  # request의 header 정보
-    print(request.content_type)  # application/json
-    print(f"######## React ID is {data} ########")
-    resp = FashionService().service_model(int(data['id']))
-    return JsonResponse({'result': resp})
+    data = json.loads(request.body)  # json to dict
+    print(f"######## GET at Here ! React ID is {data['id']} ########")
+    return JsonResponse({'result': FashionService().service_model(int(data['id']))})
