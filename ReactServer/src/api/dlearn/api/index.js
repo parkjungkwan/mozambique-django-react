6f08135790b773e20d4a9d0a3282abc38aeb50e5
parkjungkwan <pakjkwan@gmail.com>
2022-12-16 16:00:22 +0900

    16

--- ReactServer/src/api/dlearn/api/index.js
+++ ReactServer/src/api/dlearn/api/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
 import {server, dlearn, vision} from 'context'
 export const iris = req => axios.post(`${server}${dlearn}iris`, req)
-export const fashion = id => axios.get(`${server}${dlearn}fashion?id=${id}`)
+export const getFashion = id => axios.get(`${server}${dlearn}fashion?id=${id}`)
+export const postFashion = id => axios.post(`${server}${dlearn}fashion?id=${id}`)
--- ReactServer/src/api/dlearn/components/Fashion.jsx
+++ ReactServer/src/api/dlearn/components/Fashion.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { fashion } from "../api"
+import { getFashion, postFashion } from "../api"
 const Fashion = () => {
     const [inputs, setInputs] = useState({})
     const {id} = inputs
@@ -8,10 +8,10 @@ const Fashion = () => {
       const {value, name} = e.target
       setInputs({...inputs, [name]: value})
     }
-    const onClick = e => {
+    const onGetClick = e => {
         e.preventDefault()
         alert(`사용자 이름: ${JSON.stringify(request)}`)
-        fashion(id)
+        getFashion(id)
         .then((res)=>{
             alert(`옷의 카테고리 : ${JSON.stringify(res.data.result)}`)
         })
@@ -20,11 +20,31 @@ const Fashion = () => {
             alert('숫자를 다시 입력해주세요.')
         })
     }
-    return(<form method="get">
-    <h1>FASHION</h1>
+    const onPostClick = e => {
+        e.preventDefault()
+        alert(`사용자 이름: ${JSON.stringify(request)}`)
+        postFashion(id)
+        .then((res)=>{
+            alert(`옷의 카테고리 : ${JSON.stringify(res.data.result)}`)
+        })
+        .catch((err)=>{
+            console.log(err)
+            alert('숫자를 다시 입력해주세요.')
+        })
+    }
+    return(<>
+    <form method="get">
+    <h1>FASHION GET방식</h1>
     <p>카테고리를 알고 싶은 옷의 번호를 입력해주세요.</p>
     <input type="text" placeholder="테스트할 옷 번호" name="id" onChange={onChange}/>
-    <button onClick={onClick}>옷의 카테고리 찾기</button>
-    </form>)
+    <button onClick={onGetClick}>옷의 카테고리 찾기</button>
+    </form>
+    <form method="post">
+    <h1>FASHION POST방식</h1>
+    <p>카테고리를 알고 싶은 옷의 번호를 입력해주세요.</p>
+    <input type="text" placeholder="테스트할 옷 번호" name="id" onChange={onChange}/>
+    <button onClick={onPostClick}>옷의 카테고리 찾기</button>
+    </form>
+    </>)
 }
 export default Fashion
