dd8babc06cc0efa63d413160908e99b64f11c49d
parkjungkwan <pakjkwan@gmail.com>
2022-12-27 18:13:16 +0900

    18

--- DjangoServer/basic/nlp/imdb/services.py
+++ DjangoServer/basic/nlp/imdb/services.py
@@ -1,6 +1,12 @@
+import csv
+import time
+from os import path
+
+import pandas as pd
+from bs4 import BeautifulSoup
 from keras.datasets import imdb
 from keras_preprocessing.sequence import pad_sequences
-from selenium.webdriver.chrome import webdriver
+from selenium import webdriver
 from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
@@ -50,16 +56,40 @@ class ImdbService(object):
 
 class NaverMovieService(object):
     def __init__(self):
-        pass
+        global url, driver, file_name, encoding
+        url = 'https://movie.naver.com/movie/point/af/list.naver?&page='
+        driver = webdriver.Chrome(r'C:\Users\AIA\MsaProject\DjangoServer\basic\webcrawler\chromedriver.exe')
+        file_name = r'C:\Users\AIA\MsaProject\DjangoServer\basic\nlp\imdb\naver_movie_review_corpus.csv'
+        encoding = "UTF-8"
 
     def crawling(self):
-        url = 'https://movie.naver.com/movie/point/af/list.naver?&page=1'
-        driver = webdriver.Chrome(r'C:\Users\AIA\MsaProject\DjangoServer\basic\webcrawler\chromedriver.exe')
-        file_name = 'naver_movie_review_corpus.csv'
-        driver.close()
+        if not path.exists(file_name):
+            review_data = []
+            for page in range(1, 2):
+                driver.get(url + str(page))
+                soup = BeautifulSoup(driver.page_source, 'html.parser')
+                all_tds = soup.find_all('td', attrs={'class', 'title'})
+                for review in all_tds:
+                    need_reviews_cnt = 1000
+                    sentence = review.find("a", {"class": "report"}).get("onclick").split("', '")[2]
+                    if sentence != "":  # 리뷰 내용이 비어있다면 데이터를 사용하지 않음
+                        score = review.find("em").get_text()
+                        review_data.append([sentence, int(score)])
+            time.sleep(1)  # 다음 페이지를 조회하기 전 1초 시간 차를 두기
+            with open(file_name, 'w', newline='', encoding=encoding) as f:
+                wr = csv.writer(f)
+                wr.writerows(review_data)
+            driver.close()
+
+        data = pd.read_csv(file_name, header=None)
+        data.columns = ['review', 'score']
+        result = [print(f"{i + 1}. {data['score'][i]}\n{data['review'][i]}\n") for i in range(len(data))]
+        return result
+
 
 
 
 if __name__ == '__main__':
-    ImdbService().hook()
+    # ImdbService().hook()
+    NaverMovieService().crawling()
 
