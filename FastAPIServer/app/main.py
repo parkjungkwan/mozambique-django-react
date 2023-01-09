9e020ca20bac5911391856290ed726438969c3c8
parkjungkwan <pakjkwan@gmail.com>
2023-01-09 10:15:13 +0900

    10

--- FastAPIServer/app/main.py
+++ FastAPIServer/app/main.py
@@ -1,19 +1,16 @@
 import os
 import sys
-
-import uvicorn
-
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 baseurl = os.path.dirname(os.path.abspath(__file__))
 from app.api.endpoints.url import Url
-
-
-from fastapi import FastAPI
-
+from fastapi import FastAPI, APIRouter
 from app.models.user import User
+from .routers.user import router as user_router
 
+router = APIRouter()
+router.include_router(user_router, prefix="/users",tags=["users"])
 app = FastAPI()
-
+app.include_router(router)
 
 @app.get("/")
 async def root():
@@ -28,3 +25,6 @@ async def say_hello(name: str):
 async def login(user: User):
     print(f"리액트에서 넘긴 정보: {user.get_email()}, {user.get_password}")
 
+@app.get("/users")
+async def get_users():
+    return [{"user_email":"test1","passowrd":"1"},{"user_email":"test2","passowrd":"1"}]
