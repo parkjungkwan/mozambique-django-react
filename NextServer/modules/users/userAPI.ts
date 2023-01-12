73e54e5292348d5d7871012e6b993abf55c2c7d7
parkjungkwan <pakjkwan@gmail.com>
2023-01-12 12:19:19 +0900

    12

--- NextServer/modules/users/userAPI.ts
+++ NextServer/modules/users/userAPI.ts
@@ -2,10 +2,10 @@ import axios, { AxiosResponse } from 'axios'
 import {context} from '@/components/admin/enums'
 import { currentTime } from '@/components/admin/utils'
 export interface UserType{
-    userId : string
-    userEmail : string
+    userid : string
+    email : string
     password : string
-    userName : string
+    username : string
     phone : string
     birth : string
     address : string
@@ -15,7 +15,14 @@ export interface UserType{
     createAt: string
     updatedAt: string
 }
-export const joinApi = async (payload: {email: string, username: string, password: string}) => {
+export const joinApi = async (payload: { email : string,
+    password : string,
+    username : string,
+    phone : string,
+    birth : string,
+    address : string,
+    job : string,
+    userInterests : string}) => {
     try{
         const response : AxiosResponse<unknown, UserType[]> = await axios.post(`${context.server}`)
         return response.data
