93e6eff12fd43f900871c95a9aac2ea5a12ba2e6
parkjungkwan <pakjkwan@gmail.com>
2023-01-16 12:24:51 +0900

    12

--- FastAPIServer/app/admin/security.py
+++ FastAPIServer/app/admin/security.py
@@ -4,6 +4,18 @@ from typing import Union, Any
 from datetime import datetime, timedelta
 import shortuuid
 from app.admin.utils import utc_seoul
+import jwt
+from passlib.context import CryptContext
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # 7 days
+ALGORITHM = "HS256"
+# JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']     # should be kept secret
+JWT_SECRET_KEY = "JWT_SECRET_KEY"
+# JWT_REFRESH_SECRET_KEY = os.environ['JWT_REFRESH_SECRET_KEY']
+JWT_REFRESH_SECRET_KEY = "JWT_REFRESH_SECRET_KEY"
+
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def myuuid():
     alphabet = string.ascii_lowercase + string.digits
@@ -11,7 +23,7 @@ def myuuid():
     return su.random(length=8)
 
 def get_hashed_password(plain_password: str) -> str:
-    pass
+    return password_context.hash(plain_password)
 
 def verify_password(plain_password: str, hashed_password: str):
     pass
