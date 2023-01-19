a9476d6cbc20e2e91fae68936df3faf1e0ef526c
parkjungkwan <pakjkwan@gmail.com>
2023-01-19 17:13:32 +0900

    getlist user

--- NextServer/src/components/Home.tsx
+++ NextServer/src/components/Home.tsx
@@ -1,14 +1,46 @@
-import {useState} from "react"
+import axios from "axios";
+import {useState, useEffect} from "react"
 import Pagination from "./admin/Pagination";
+
 
 
 export default function Home(){
 
-    const [currentPage, setCurrentPage] = useState(1);
-  const lastPage = 3;
+   const [currentPage, setCurrentPage] = useState(1);
+   const lastPage = 3;
+   const [list, setList] = useState([])
+
+    useEffect(()=>{
+        axios
+        .get('http://localhost:8000/users/page/1')
+        .then(res => {
+            const items = res.data.items
+            setList(items)
+        })
+        .catch(err => {console.log(err)})
+    }, [])
 
   return (
-    <div className="page-container">
+    <>
+    <h2>User List</h2>
+        <table className='user-list'>
+            <thead>
+                <tr>
+                <th>ID</th><th>이메일</th><th>비번</th><th>이름</th><th>전화번호</th>
+                <th>생년월일</th><th>주소</th><th>직업</th><th>관심사항</th>
+                </tr>
+            </thead>
+            <tbody>
+            {list && list.map(({userid, email, password, username, phone, birth, address, job, interests})=>(
+                <tr key={userid}>
+                    <td>{userid}</td><td>{email}</td><td>{password}</td><td>{username}</td>
+                    <td>{phone}</td><td>{birth}</td><td>{address}</td>
+                    <td>{job}</td><td>{interests}</td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+        <div className="page-container">
       <h1>React TypeScript Pagination</h1>
       <Pagination
         currentPage={currentPage}
@@ -17,5 +49,7 @@ export default function Home(){
         setCurrentPage={setCurrentPage}
       />
     </div>
+    </>
+    
   );
 }
