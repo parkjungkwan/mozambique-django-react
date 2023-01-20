1e048ee56ccffead341b1dc87641c6431a7dcc69
parkjungkwan <pakjkwan@gmail.com>
2023-01-20 16:22:52 +0900

    16:22

--- FastAPIServer/app/admin/pagination.py
+++ FastAPIServer/app/admin/pagination.py
@@ -13,19 +13,28 @@ router = APIRouter()
 def pagination(request_page: int, db: Session = Depends(get_db)):
     row_cnt = UserCrud(db).count_all_users()
     page_size = 10
-    response_page = request_page - 1  # 넘겨받은 page번호를 인덱스 값으로 전환
-    t = row_cnt // page_size
-    t2 = row_cnt % page_size
-    page_cnt = t if (t2 == 0) else t + 1
-    t3 = page_cnt // page_size
     block_size = 10
-    t4 = page_cnt % block_size
-    block_cnt = t3 if (t4 == 0) else t3 + 1
+    response_page = request_page - 1  # 넘겨받은 page번호를 인덱스 값으로 전환
+    page_cnt_mok = row_cnt // page_size
+    page_cnt_nmg = row_cnt % page_size
+    page_cnt = page_cnt_mok if (page_cnt_nmg == 0) else page_cnt_mok + 1
+    block_cnt_mok = page_cnt // page_size
+    block_cnt_nmg = page_cnt % block_size
+    block_cnt = block_cnt_mok if (block_cnt_nmg == 0) else block_cnt_mok + 1
     start_row_per_page = page_size * (response_page)
     response_block = (response_page) // block_size
-    end_row_per_page = start_row_per_page + (page_size - 1) if request_page != page_cnt else row_cnt - 1
+    last_row_idx_per_total = row_cnt - 1
+    last_row_idx_per_page = page_size - 1
+    end_row_per_page = start_row_per_page + last_row_idx_per_page \
+        if request_page != page_cnt \
+        else last_row_idx_per_total
     start_page_per_block = response_block * block_size
-    end_page_per_block = start_page_per_block + (block_size -1 ) if response_block != (block_cnt - 1) else page_cnt
+    last_page_idx_per_total = page_cnt - 1
+    last_block_idx_per_total = block_cnt - 1
+    last_page_idx_per_block = block_size - 1
+    end_page_per_block = start_page_per_block + last_page_idx_per_block \
+        if response_block != last_block_idx_per_total \
+        else last_page_idx_per_total
 
     print("### 테스트 ### ")
     print(f"start_row_per_page ={start_row_per_page}")
