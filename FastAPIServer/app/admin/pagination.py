d16ba92b12e56587a7747212b2775561202f280b
parkjungkwan <pakjkwan@gmail.com>
2023-01-20 15:55:36 +0900

    16:00

--- FastAPIServer/app/admin/pagination.py
+++ FastAPIServer/app/admin/pagination.py
@@ -11,29 +11,37 @@ from app.schemas.user import UserDTO
 
 router = APIRouter()
 
-@router.get("/page/{page}")
-def pagination(page: int, db: Session = Depends(get_db)):
+@router.get("/page/{request_page}")
+def pagination(request_page: int, db: Session = Depends(get_db)):
     row_cnt = UserCrud(db).count_all_users()
-    page_size = 5
+    page_size = 10
+    response_page = request_page - 1  # 넘겨받은 page번호를 인덱스 값으로 전환
     t = row_cnt // page_size
     t2 = row_cnt % page_size
     page_cnt = t if (t2 == 0) else t + 1
     t3 = page_cnt // page_size
-    block_size = 5
+    block_size = 10
     t4 = page_cnt % block_size
     block_cnt = t3 if (t4 == 0) else t3 + 1
-    page_now = page
-    row_start = page_size * (page_now - 1)
-    block_now = page_now // block_size
-    row_end = row_start + (page_size - 1) if page_now != page_cnt else row_cnt - 1
-    page_start = block_now * block_size
-    page_end = page_start + (block_size -1 ) if block_now != (block_cnt - 1) else page_cnt
+    start_row_per_page = page_size * (response_page)
+    response_block = (response_page) // block_size
+    end_row_per_page = start_row_per_page + (page_size - 1) if request_page != page_cnt else row_cnt - 1
+    start_page_per_block = response_block * block_size
+    end_page_per_block = start_page_per_block + (block_size -1 ) if response_block != (block_cnt - 1) else page_cnt
 
     print("### 테스트 ### ")
-    print(f"row_start ={row_start}")
-    print(f"row_end ={row_end}")
-    print(f"page_start ={page_start}")
-    print(f"page_end ={page_end}")
+    print(f"start_row_per_page ={start_row_per_page}")
+    print(f"end_row_per_page ={end_row_per_page}")
+    print(f"start_page_per_block ={start_page_per_block}")
+    print(f"end_page_per_block ={end_page_per_block}")
+
+    return {
+        "start_row_per_page" : start_row_per_page,
+        "end_row_per_page" : end_row_per_page,
+        "start_page_per_block" : start_page_per_block,
+        "end_page_per_block" : end_page_per_block,
+        "response_block" : response_block
+    }
 
 
     '''
@@ -61,9 +69,9 @@ def insert_many(db: Session = Depends(get_db)):
     temp = UserDTO(email=faker.email(),password="11aa",
                   username=faker.name(), birth=between_random_date(),
                   address=faker.city())
-    [ UserCrud(db).add_user(
-        )
-        for i in range(100)]
+    print(f" Faker 작동 ")
+    UserCrud(db).add_user(temp)
+    # [UserCrud(db).add_user(temp)for i in range(100)]
     """
     [print(UserFaker(
         email=faker.email(),
--- FastAPIServer/app/admin/utils.py
+++ FastAPIServer/app/admin/utils.py
@@ -30,5 +30,27 @@ def between_random_date():
     target = str(random_date(d1, d2))
     return target.split()[0]
 
+def paging(page: int, row_cnt: int): # row_cnt = UserCrud(db).count_all_users()
+    page_size = 10
+    t = row_cnt // page_size
+    t2 = row_cnt % page_size
+    page_cnt = t if (t2 == 0) else t + 1
+    t3 = page_cnt // page_size
+    block_size = 10
+    t4 = page_cnt % block_size
+    block_cnt = t3 if (t4 == 0) else t3 + 1
+    page_now = page
+    row_start = page_size * (page_now - 1)
+    block_now = page_now // block_size
+    row_end = row_start + (page_size - 1) if page_now != page_cnt else row_cnt - 1
+    page_start = block_now * block_size
+    page_end = page_start + (block_size -1 ) if block_now != (block_cnt - 1) else page_cnt
+
+    print("### 테스트 ### ")
+    print(f"row_start ={row_start}")
+    print(f"row_end ={row_end}")
+    print(f"page_start ={page_start}")
+    print(f"page_end ={page_end}")
+
 if __name__ == '__main__':
     print(f"랜덤 생일 : {between_random_date()}")
