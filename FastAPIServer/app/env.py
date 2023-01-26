3c633484fe1b60fcf854d3eab65fdc4e48676dd7
parkjungkwan <pakjkwan@gmail.com>
2023-01-26 16:58:36 +0900

    update gitignore

--- FastAPIServer/app/env.py
+++
@@ -1,7 +0,0 @@
-USERNAME = "mydb"
-PASSWORD = "rootroot"
-HOSTNAME = "mydb.cze3kaeuqyxd.ap-northeast-2.rds.amazonaws.com"
-DATABASE = "mydb"
-PORT = 3306
-CHARSET = "utf8"
-DB_URL = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}"
